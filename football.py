adc8a1e0c9ce59091a6bd172986e2f05e51b35d4
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-02-25 22:52:33 +0300

    Update football.py

--- football.py
+++ football.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import urllib
+from CodeList import dict
+#print dict().test()
 class Code(object):
     def __init__(self, code):
         self.code = str(code)
@@ -17,7 +20,7 @@ class Code(object):
             if test.find(search) is not -1:
                 pos = self.getstr(test, ">","<")
                 if self.source[e+1].find("deltaStand") is not -1:
-                    self.delta.append("Stand") 
+                    self.delta.append("Stand")
                 elif self.source[e+1].find("deltaDown") is not -1:
                     self.delta.append("Down")
                 elif self.source[e+1].find("deltaUp") is not -1:
@@ -27,21 +30,22 @@ class Code(object):
                 print self.getstr(self.source[e+15], ">", "<")
                 self.points.append(points)
                 string = self.source[e+3]
-                if string.find(self.Teams()["Ev"]) is not -1:
+                if string.find(self.Teams()["HTTR"]) is not -1:
                     self.posHTTR = pos
-                if string.find(self.Teams()["Konuk"]) is not -1:
+                if string.find(self.Teams()["ATTR"]) is not -1:
                     self.posATTR = pos
-        self.posDict["sira"] = {"Ev":self.posHTTR, "Konuk":self.posATTR}
+        self.posDict["Pos"] = {"HTTR":self.posHTTR, "ATTR":self.posATTR}
         self.posHTTR = int(self.posHTTR)
         self.posATTR = int(self.posATTR)
-        self.posDict["Takimlar"] = {"Ev":get.Teams()["Ev"],  "Konuk":get.Teams()["Konuk"]}
-        self.posDict["delta"] = {"Ev":self.delta[self.posHTTR-1], "Konuk":self.delta[self.posATTR-1]}
-        self.posDict["puan"] = {"Ev":self.points[self.posHTTR-1], "Konuk":self.points[self.posATTR-1]}
-        self.posDict["hava"] = {"stat":get.weather()["stat"].decode("utf-8"), "derece":get.weather()["c"]}
-        self.posDict["kral"] = {"puan":self.points[self.zero]}
+        self.posDict["TEAMS"] = {"HTTR":get.Teams()["HTTR"],  "ATTR":get.Teams()["ATTR"]}
+        self.posDict["Delta"] = {"HTTR":self.delta[self.posHTTR-1], "ATTR":self.delta[self.posATTR-1]}
+        self.posDict["Point"] = {"HTTR":self.points[self.posHTTR-1], "ATTR":self.points[self.posATTR-1]}
+        self.posDict["Weather"] = {"Stat":get.weather()["stat"].decode("utf-8"), "Temp":get.weather()["c"]}
+        self.posDict["Kral"] = {"point":self.points[self.zero]}
         self.posDict["Ts"] = {"count":self.Count()}
         self.posDict["Lig"] = {"Lig":self.lig()}
-        self.posDict["Averaj"] = {"Ev":self.averageList[self.posHTTR-1], "Konuk":self.averageList[self.posATTR-1]}
+        self.posDict["Averaj"] = {"HTTR":self.averageList[self.posHTTR-1], "ATTR":self.averageList[self.posATTR-1],
+        "Fark":self.averageList[self.posHTTR-1]+self.averageList[self.posATTR-1]}
         return self.posDict
     def lig(self):
         for test in self.source:
@@ -59,7 +63,7 @@ class Code(object):
         return self.dict
     def getstr(self, source, start, end):
         get = source.find(start) +1
-        output = source.find(end, get) 
+        output = source.find(end, get)
         return source[get:output]
     def Count(self):
         for test in self.source:
@@ -72,15 +76,13 @@ class Code(object):
             search = 'Nesine - ' + self.code + ' Kodlu'
             if test.find(search) is not -1:
                 Teams = test.strip()[len(search):].replace("Maçı","").replace(" - ","-").strip().split("-")
-        self.dict = {"Ev":Teams[0], "Konuk":Teams[1]}
-        return self.dict 
-string = """Ligin {}. sırasında olan {}. sıradaki ev sahibi {} takımı 
-evinde {}.sıradaki {} takımını konuk ediyor. Ev sahibi ile aralarında {} puan 
+        self.dict = {"HTTR":Teams[0], "ATTR":Teams[1]}
+        return self.dict
+string = """Ligin {}. sırasında olan {}. sıradaki ev sahibi {} takımı
+evinde {}.sıradaki {} takımını konuk ediyor. Ev sahibi ile aralarında {} puan
 farkı olan {} takımı """
 #string = string.format("test")
 
-get = Code(150)
-#print get.Teams()["Ev"], get.Count(), get.weather()["stat"], get.weather()["c"]
+get = Code(623)
+#print get.Teams()["HTTR"], get.Count(), get.weather()["Stat"], get.weather()["Temp"]
 print get.generic()
-
-        
